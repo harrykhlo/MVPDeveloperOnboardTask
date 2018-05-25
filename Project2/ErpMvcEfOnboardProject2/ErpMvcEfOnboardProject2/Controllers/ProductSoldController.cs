04f24a1cb7efde10b4cb64521c6ccf8a1f200d44
Harry <harrykhlo@gmail.com>
2018-05-25 12:14:37 +1200

    leave getbyId() and Update() functions commented in ProductSold.js and ProductSoldController.cs of Project 2 because they do not work. The others are working well

---
+++ Project2/ErpMvcEfOnboardProject2/ErpMvcEfOnboardProject2/Controllers/ProductSoldController.cs
@@ -0,0 +1,98 @@
+﻿using ErpMvcEfOnboardProject2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErpMvcEfOnboardProject2.Controllers
+{
+    public class ProductSoldController : Controller
+    {
+        public class ProductSoldViewModel
+        {
+
+            public int Id { get; set; }
+            public long ProductId { get; set; }
+            public long CustomerId { get; set; }
+            public int StoreId { get; set; }
+            public string DateSold { get; set; }
+        }
+
+        // database object
+        private ErpMvcEfOnboardTaskEntities db = new ErpMvcEfOnboardTaskEntities();
+
+        // GET: ProductSold
+        public ActionResult Index()
+        {
+            var psold = new ProductSold();
+            //ViewBag.CustomerId = new SelectList(db.Customers, "Id", "CustomerName", psold.CustomerId);
+            //ViewBag.ProductId = new SelectList(db.Products, "Id", "ProductName", psold.ProductId);
+            //ViewBag.StoreId = new SelectList(db.Stores, "Id", "StoreName", psold.StoreId);
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", psold.CustomerId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", psold.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", psold.StoreId);
+
+            return View();
+        }
+
+        public JsonResult List()
+        {
+            var productSolds = db.ProductSolds.Include(p => p.Customer).Include(p => p.Product).Include(p => p.Store).Select(x => new 
+            {
+                Id = x.Id,
+                //CustomerId = x.CustomerId,
+                CustomerName = x.Customer.Name,
+                //ProductId = x.ProductId,
+                ProductName = x.Product.Name,
+                //StoreId = x.StoreId,
+                StoreName = x.Store.Name,
+                Day = x.DateSold.Day,
+                Month = x.DateSold.Month,
+                Year = x.DateSold.Year
+            }).ToList();
+
+            return Json(productSolds, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult Add(ProductSold psold)
+        {
+            db.ProductSolds.Add(psold);
+            db.SaveChanges();
+
+            return Json(db.SaveChanges(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult RemoveProduct(long ID)
+        {
+            ProductSold psold = db.ProductSolds.Find(ID);
+            db.ProductSolds.Remove(psold);
+            return Json(db.SaveChanges(), JsonRequestBehavior.AllowGet);
+        }
+
+        //public JsonResult GetbyID(int ID)
+        //{
+        //    //var ProductSolds = db.ProductSolds.Where(x => x.Id == ID).Select(x => new ProductSoldViewModel
+        //    var ProductSolds = db.ProductSolds.Where(x => x.Id == ID).Select(x => new 
+        //    {
+        //        Id = ID,
+        //        CustomerId = x.CustomerId,
+        //        StoreId = x.StoreId,
+        //        ProductId = x.ProductId,
+        //        //DateSold = x.DateSold.Day + "/" + x.DateSold.Month + "/" + x.DateSold.Year
+        //        DateSold = x.DateSold
+        //    }).FirstOrDefault();
+        //    return Json(ProductSolds, JsonRequestBehavior.AllowGet);
+        //}
+
+        //public JsonResult Update(ProductSold psold)
+        //{
+        //    db.Entry(psold).State = EntityState.Modified;
+        //    db.SaveChanges();
+        //    return Json(db.SaveChanges(), JsonRequestBehavior.AllowGet);
+        //}
+
+    }
+}
+
---
+++ Project2/ErpMvcEfOnboardProject2/ErpMvcEfOnboardProject2/Scripts/ProductSold.js
@@ -0,0 +1,181 @@
+﻿//Load Data in Table
+$(document).ready(function () {
+    loadData();
+});
+
+//Load Data
+function loadData() {
+    $.ajax({
+        url: "/ProductSold/List",
+        type: "GET",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            var html = '';
+            $.each(result, function (key, item) {
+                html += '<tr>';
+                html += '<td>' + item.Id + '</td>';
+                html += '<td>' + item.ProductName + '</td>';
+                html += '<td>' + item.CustomerName + '</td>';
+                html += '<td>' + item.StoreName + '</td>';
+                html += '<td>' + item.Day + '&#47;' + item.Month + '&#47;' + item.Year + '</td>';               
+                html += '<td><a href="#" onclick="return getbyID(' + item.Id + ')"<button type="button" class="btn btn-warning" > <span class = "glyphicon glyphicon-edit"></span></td>';
+                html += '<td><a href="#" onclick="Delete(' + item.Id + ')"<button type="button" class="btn btn-danger" > <span class = "glyphicon glyphicon-trash"></span></td>';
+                html += '</tr>';
+            });
+            $('.tbody').html(html);
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+//Add Data Function 
+function Add() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var psoldObj = {
+        Id: $('#Id').val(),
+        ProductId: $('#ProductId').val(),
+        CustomerId: $('#CustomerId').val(),
+        StoreId: $('#StoreId').val(),
+        DateSold: $('#DateSold').val(),
+    };
+    $.ajax({
+        url: "/ProductSold/Add",
+        data: JSON.stringify(psoldObj),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            loadData();
+            $('#myModalCreate').modal('hide');
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+//Valdidation using jquery
+function validate() {
+    var isValid = true;
+    if ($('#ProductId').val().trim() == "") {
+        $('#ProductId').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#ProductId').css('border-color', 'lightgrey');
+    }
+    if ($('#CustomerId').val().trim() == "") {
+        $('#CustomerId').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#CustomerId').css('border-color', 'lightgrey');
+    }
+    if ($('#StoreId').val().trim() == "") {
+        $('#StoreId').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#StoreId').css('border-color', 'lightgrey');
+    }
+
+    if ($('#DateSold').val().trim() == "") {
+        $('#DateSold').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#DateSold').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
+
+////Get the Data Based on ProductSold Id
+//function getbyID(psoldId) {
+//    $('#ProductId').css('border-color', 'lightgrey');
+//    $('#CustomerId').css('border-color', 'lightgrey');
+//    $('#StoreId').css('border-color', 'lightgrey');
+//    $('#DateSold').css('border-color', 'lightgrey');
+
+//    $.ajax({
+//        url: "/ProductSold/getbyID/" + psoldId,
+//        type: "GET",
+//        contentType: "application/json;charset=UTF-8",
+//        dataType: "json",
+//        success: function (result) {
+//            $('#Id').val(result.Id);
+//            $('#ProductName').val(result.ProductName);
+//            $('#CustomerName').val(result.CustomerName);
+//            $('#StoreName').val(result.StoreName);
+//            $('#DateSold').val(result.DateSold);
+
+//            $('#myModalEdit').modal('show');
+//            $('#btnUpdate').show();
+//            $('#btnAdd').hide();
+//        },
+//        error: function (errormessage) {
+//            alert(errormessage.responseText);
+//        }
+//    });
+//    return false;
+//}
+
+//Delete Product Sold's record
+function Delete(ID) {
+    var ans = confirm("Are you sure you want to delete this Record?");
+    if (ans) {
+        $.ajax({
+            url: "/ProductSold/RemoveProduct/" + ID,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                loadData();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+
+////Update Product Sold's record
+//function Update() {
+//    var res = validate();
+//    if (res == false) {
+//        return false;
+//    }
+//    var psoldObj = {
+//        Id: $('#Id').val(),
+//        ProductId: $('#ProductId').val(),
+//        CustomerId: $('#CustomerId').val(),
+//        StoreId: $('#StoreId').val(),
+//        DateSold: $('#DateSold').val(),
+
+//    };
+//    $.ajax({
+//        url: "/ProductSold/Update",
+//        data: JSON.stringify(psoldObj),
+//        type: "POST",
+//        contentType: "application/json;charset=utf-8",
+//        dataType: "json",
+//        success: function (result) {
+//            loadData();
+//            $('#myModalEdit').modal('hide');
+//            $('#Id').val("");
+//            $('#ProductId').val("");
+//            $('#CustomerId').val("");
+//            $('#StoreId').val("");
+//            $('#DateSold').val("");
+
+//        },
+//        error: function (errormessage) {
+//            alert(errormessage.responseText);
+//        }
+//    });
+//}
